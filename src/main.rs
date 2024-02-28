b3880cd96ed61a57bafda22529dc5b117ac76857
amatgil <amatgilvinyes@gmail.com>
2024-02-28 02:05:03 +0100

    The bulk of the work has been done, play time!

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use ppmitzador::*;
 
 const BG: Pixel        = Pixel::BLACK;
 const TRI_COLOR: Pixel = Pixel::WHITE;
-const WIDTH: usize     = 1000;
+const WIDTH: usize     = 2000;
 const HEIGHT: usize    = WIDTH;
-const ANTI: bool       = false;
 
 fn lerp_c(a: Coord, b: Coord, t: f64) -> Coord {
     // x
@@ -16,22 +15,20 @@ fn lerp_c(a: Coord, b: Coord, t: f64) -> Coord {
     Coord { x, y }
 }
 
-fn main() {
-    let n = 1;
+fn save_flake(n: usize, anti: bool) {
     let mut data = ImagePPM::new(WIDTH, HEIGHT, BG);
 
     let mut points = vec![
         Coord { x: 1*WIDTH/4, y: 1*HEIGHT / 4 },
-        Coord { x: 2*WIDTH/4, y: 3*HEIGHT / 4 },
+        //Coord { x: 2*WIDTH/4, y: 3*HEIGHT / 4 },
         Coord { x: 3*WIDTH/4, y: 1*HEIGHT / 4 },
-        Coord { x: 1*WIDTH/4, y: 1*HEIGHT / 4 }, // repetida pel cicle
+        //Coord { x: 1*WIDTH/4, y: 1*HEIGHT / 4 }, // repetida pel cicle
     ];
 
     for _ in 0..n {
         let mut new_points = vec![];
         
         for pair in points.windows(2) {
-            println!("Doing pair: {pair:?}");
             //        d
             //       / \            <- Koch pattern
             // a -- c   e -- b
@@ -51,32 +48,31 @@ fn main() {
                 let dx = x2 - x1;
                 let dy = y2 - y1;
 
-                let theta = TAU/6.0 * if ANTI { 1.0 } else { -1.0 }; // 60ª
+                let theta = TAU/6.0 * if anti { 1.0 } else { -1.0 }; // 60ª
 
                 let rx = x1 +    theta.cos()*dx + theta.sin()*dy;
                 let ry = y1 + (-theta).sin()*dx + theta.cos()*dy;
                 
                 Coord { x: rx as usize, y: ry as usize}
             };
-
-            new_points.push(a);
-            new_points.push(c);
-            new_points.push(d);
-            new_points.push(e);
-            new_points.push(b);
-
-    
+            new_points.extend([a, c, d, e, b]);
         }
         points = new_points
     }
 
-    for pair in points.windows(2) { data.draw_line_with_thickness(pair[0], pair[1], TRI_COLOR, 10); }
+    for pair in points.windows(2) { data.draw_line_with_thickness(pair[0], pair[1], TRI_COLOR, 2); }
 
-    //data.draw_circle(Coord { x: WIDTH / 2, y: HEIGHT / 2 }, 100, Pixel::PURPLE);
+    let filename = format!("line.ppm");
+    data.save_to_file(&filename).unwrap();
+    println!("When n = {n}, there were {} points", points.len());
 
-    println!("Saving to file");
-    data.save_to_file("testing.ppm").unwrap();
-    println!("Saved");
+    println!("Saved '{}'", filename);
+}
 
-    println!("Finished");
+fn main() {
+    //for n in 0..15 {
+    for t in [false, true] {
+        save_flake(5, t);
+    }
+    //}
 }
