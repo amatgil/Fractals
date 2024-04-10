d9457e28bdbe329c3201b0b483ccb97e10993994
amatgil <amatgilvinyes@gmail.com>
2024-04-10 16:59:59 +0200

    ran cargofmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,12 @@ use ppmitzador::Coord;
 
 use wasm_bindgen::prelude::*;
 
-
-const WIDTH: usize  = 1024;
+const WIDTH: usize = 1024;
 const HEIGHT: usize = WIDTH;
 const COLOR: &str = "#f0c6c6";
 
 // Von Koch constants
-const THETA: f64 = TAU/6.0;
+const THETA: f64 = TAU / 6.0;
 
 // Wasm "constants"
 pub fn set_panic_hook() {
@@ -19,60 +18,75 @@ pub fn set_panic_hook() {
 
 // Misc
 fn lerp_c(a: Coord, b: Coord, t: f64) -> Coord {
-    let x = ((a.x as f64)*(1.0 - t) + (b.x as f64)*t).round() as usize;
-    let y = ((a.y as f64)*(1.0 - t) + (b.y as f64)*t).round() as usize;
+    let x = ((a.x as f64) * (1.0 - t) + (b.x as f64) * t).round() as usize;
+    let y = ((a.y as f64) * (1.0 - t) + (b.y as f64) * t).round() as usize;
 
     Coord { x, y }
 }
 
-
 // Von Koch Functions
 fn get_flake_points(n: usize, anti: bool) -> Vec<Coord> {
-    if n > 9 { panic!("n values above 8 make svg viewers struggle, so I'm disallowing them") }
+    if n > 9 {
+        panic!("n values above 8 make svg viewers struggle, so I'm disallowing them")
+    }
     println!("[INFO]: Initializing...");
-    let lowest_y  = (0.5 - (3.0 as f64).sqrt()/12.0)*(HEIGHT as f64);
-    let highest_y = lowest_y + (3.0 as f64).sqrt()*(HEIGHT as f64)/4.0;
+    let lowest_y = (0.5 - (3.0 as f64).sqrt() / 12.0) * (HEIGHT as f64);
+    let highest_y = lowest_y + (3.0 as f64).sqrt() * (HEIGHT as f64) / 4.0;
 
     let mut points = vec![
-        Coord { x: 1*WIDTH/4, y: lowest_y  as usize},
-        Coord { x: 2*WIDTH/4, y: highest_y as usize},
-        Coord { x: 3*WIDTH/4, y: lowest_y  as usize},
-        Coord { x: 1*WIDTH/4, y: lowest_y  as usize}, // repetida pel cicle
+        Coord {
+            x: 1 * WIDTH / 4,
+            y: lowest_y as usize,
+        },
+        Coord {
+            x: 2 * WIDTH / 4,
+            y: highest_y as usize,
+        },
+        Coord {
+            x: 3 * WIDTH / 4,
+            y: lowest_y as usize,
+        },
+        Coord {
+            x: 1 * WIDTH / 4,
+            y: lowest_y as usize,
+        }, // repetida pel cicle
     ];
 
     println!("[INFO]: Computating for n = {n}...");
     for _ in 0..n {
         let mut new_points = vec![];
-        
+
         for pair in points.windows(2) {
             //        d
             //       / \            <- Koch pattern
             // a -- c   e -- b
             //
-            // Here, theta is the angle fomed by the segments c-d and d-e 
+            // Here, theta is the angle fomed by the segments c-d and d-e
 
             let a = pair[0];
             let b = pair[1];
 
-            let c = lerp_c(a, b,       0.5/(1.0 + (THETA/2.0).sin()));
-            let e = lerp_c(a, b, 1.0 - 0.5/(1.0 + (THETA/2.0).sin()));
+            let c = lerp_c(a, b, 0.5 / (1.0 + (THETA / 2.0).sin()));
+            let e = lerp_c(a, b, 1.0 - 0.5 / (1.0 + (THETA / 2.0).sin()));
 
-            // place d |c-a|*cos(theta/2) units above the midpoint of a, b 
-            let d = { 
-
+            // place d |c-a|*cos(theta/2) units above the midpoint of a, b
+            let d = {
                 // find the midpoint of a and b
                 let mid = lerp_c(a, b, 0.5);
-                
+
                 // find vector from a to c
-                let ca_vec_x = (c.x as f64 - a.x as f64)*if anti {-1.0} else {1.0};
-                let ca_vec_y = (c.y as f64 - a.y as f64)*if anti {-1.0} else {1.0};
-                
+                let ca_vec_x = (c.x as f64 - a.x as f64) * if anti { -1.0 } else { 1.0 };
+                let ca_vec_y = (c.y as f64 - a.y as f64) * if anti { -1.0 } else { 1.0 };
+
                 // place d |c-a|*cos(theta/2) units perpendicular to the c-a vector
                 // this comes from the c-d-mid right angle triangle where |d-c| = |c-a|
-                let dx = mid.x as f64 - (THETA/2.0).cos()*(ca_vec_y as f64);
-                let dy = mid.y as f64 + (THETA/2.0).cos()*(ca_vec_x as f64);
-                
-                Coord { x: dx as usize, y: dy as usize}
+                let dx = mid.x as f64 - (THETA / 2.0).cos() * (ca_vec_y as f64);
+                let dy = mid.y as f64 + (THETA / 2.0).cos() * (ca_vec_x as f64);
+
+                Coord {
+                    x: dx as usize,
+                    y: dy as usize,
+                }
             };
             new_points.extend([a, c, d, e, b]);
         }
@@ -93,7 +107,7 @@ pub fn vonkoch(n: usize, anti: bool) -> String {
     buffer.push_str(&format!("<svg viewBox=\"0 0 {WIDTH} {HEIGHT} \" xmlns=\"http://www.w3.org/2000/svg\" id=\"vonkoch-holder\">\n"));
     buffer.push_str(&format!("<polygon points=\""));
     for Coord { x, y } in points {
-	buffer.push_str(&format!("{x},{y} "));
+        buffer.push_str(&format!("{x},{y} "));
     }
     buffer.push_str(&format!("\" fill=\"none\" stroke=\"{COLOR}\"/>\n"));
     buffer.push_str(&format!("</svg>\n"));
@@ -104,26 +118,47 @@ pub fn vonkoch(n: usize, anti: bool) -> String {
 // Sierp functions
 #[wasm_bindgen]
 pub fn sierp(n: usize) -> String {
-    let lowest_y  = (0.5 - (3.0 as f64).sqrt()/12.0)*(HEIGHT as f64);
-    let highest_y = lowest_y + (3.0 as f64).sqrt()*(HEIGHT as f64)/4.0;
+    let lowest_y = (0.5 - (3.0 as f64).sqrt() / 12.0) * (HEIGHT as f64);
+    let highest_y = lowest_y + (3.0 as f64).sqrt() * (HEIGHT as f64) / 4.0;
     let lowest_y = lowest_y as usize;
     let highest_y = highest_y as usize;
 
-
-
-    let base_side_length = Coord { x: WIDTH/4 , y: highest_y - lowest_y };
+    let base_side_length = Coord {
+        x: WIDTH / 4,
+        y: highest_y - lowest_y,
+    };
 
     let mut points = Vec::new();
-    sierp_go(n, &mut points, Coord { x: WIDTH/4, y: lowest_y as usize}, base_side_length);
-    points = points.into_iter().map(|triplet| triplet.map(|p| Coord { x: p.x, y: HEIGHT - p.y - 1 })).collect(); // Flip vertically
+    sierp_go(
+        n,
+        &mut points,
+        Coord {
+            x: WIDTH / 4,
+            y: lowest_y as usize,
+        },
+        base_side_length,
+    );
+    points = points
+        .into_iter()
+        .map(|triplet| {
+            triplet.map(|p| Coord {
+                x: p.x,
+                y: HEIGHT - p.y - 1,
+            })
+        })
+        .collect(); // Flip vertically
 
     let mut buffer = String::new();
 
     buffer.push_str(&format!("<svg viewBox=\"0 0 {WIDTH} {HEIGHT} \" xmlns=\"http://www.w3.org/2000/svg\" id=\"sierp-holder\">\n"));
     for triplet in points {
-	buffer.push_str(&format!("<polygon points=\""));
-	for Coord { x, y } in triplet {buffer.push_str(&format!("{x},{y} "));}
-	buffer.push_str(&format!("\" fill=\"{COLOR}\" stroke=\"#000000\" stroke-width=\"0.1%\"/>\n"));
+        buffer.push_str(&format!("<polygon points=\""));
+        for Coord { x, y } in triplet {
+            buffer.push_str(&format!("{x},{y} "));
+        }
+        buffer.push_str(&format!(
+            "\" fill=\"{COLOR}\" stroke=\"#000000\" stroke-width=\"0.1%\"/>\n"
+        ));
     }
     buffer.push_str(&format!("</svg>\n"));
 
@@ -139,19 +174,31 @@ pub fn sierp_go(n: usize, points: &mut Vec<[Coord; 3]>, start_pos: Coord, side_l
     // a ----- b     a = start_pos
 
     let c = start_pos + side_length;
-    let b = start_pos + Coord { x: side_length.x*2, y: 0 };
+    let b = start_pos
+        + Coord {
+            x: side_length.x * 2,
+            y: 0,
+        };
 
     if n <= 0 {
-	points.push([start_pos, c, b]);
-    } else {
-	let half_side_length = Coord { x: side_length.x / 2, y: side_length.y / 2 };
+        points.push([start_pos, c, b]);
+	return;
+    } 
 
-	let c = start_pos + half_side_length;
-	let b = start_pos + Coord { x: half_side_length.x*2, y: 0 };
-	sierp_go(n - 1, points, start_pos, half_side_length);
-	sierp_go(n - 1, points, c, half_side_length);
-	sierp_go(n - 1, points, b, half_side_length);
-    }
+    let half_side_length = Coord {
+	x: side_length.x / 2,
+	y: side_length.y / 2,
+    };
+
+    let c = start_pos + half_side_length;
+    let b = start_pos
+        + Coord {
+            x: half_side_length.x * 2,
+            y: 0,
+        };
+    sierp_go(n - 1, points, start_pos, half_side_length);
+    sierp_go(n - 1, points, c, half_side_length);
+    sierp_go(n - 1, points, b, half_side_length);
 }
 
 // Tree functions
@@ -159,28 +206,35 @@ pub fn sierp_go(n: usize, points: &mut Vec<[Coord; 3]>, start_pos: Coord, side_l
 #[derive(Debug, Clone)]
 struct Line {
     a: Coord,
-    b: Coord
+    b: Coord,
 }
-
 
 #[wasm_bindgen]
 pub fn tree(n: usize, theta: f64, branch_length: usize) -> String {
     let mut lines = Vec::new();
-    let first  = Coord { x: WIDTH / 2, y: 2*HEIGHT / 3};
-    let second = Coord { x: first.x, y: first.y - branch_length};
-    lines.push(Line {a: first, b: second});
+    let first = Coord {
+        x: WIDTH / 2,
+        y: 2 * HEIGHT / 3,
+    };
+    let second = Coord {
+        x: first.x,
+        y: first.y - branch_length,
+    };
+    lines.push(Line {
+        a: first,
+        b: second,
+    });
 
     tree_go(n, &mut lines, theta, branch_length, second);
-    //lines = lines.into_iter().map(|line| Line {
-    //	a: Coord { x: line.a.x, y: HEIGHT - line.a.y - 1 },
-    //	b: Coord { x: line.b.x, y: HEIGHT - line.b.y - 1 },
-    //}).collect(); // Flip vertically
 
     let mut buffer = String::new();
 
     buffer.push_str(&format!("<svg viewBox=\"0 0 {WIDTH} {HEIGHT} \" xmlns=\"http://www.w3.org/2000/svg\" id=\"tree-holder\">\n"));
     for line in lines {
-	buffer.push_str(&format!("<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"{COLOR}\" stroke-width=\"5\"/>", line.a.x, line.a.y, line.b.x, line.b.y));
+        buffer.push_str(&format!(
+            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"{COLOR}\" stroke-width=\"5\"/>",
+            line.a.x, line.a.y, line.b.x, line.b.y
+        ));
     }
     buffer.push_str(&format!("</svg>\n"));
 
@@ -188,18 +242,27 @@ pub fn tree(n: usize, theta: f64, branch_length: usize) -> String {
 }
 
 fn tree_go(n: usize, mut v: &mut Vec<Line>, theta: f64, branch_length: usize, r: Coord) {
-    if n <= 0 { return; }
+    if n <= 0 {
+        return;
+    }
 
     let dx = (branch_length as f64 * theta.cos()).round() as usize;
     let dy = (branch_length as f64 * theta.sin()).round() as usize;
-    let dtheta = 0.2;
+    let dtheta = 0.5;
 
-    let a = Coord { x: r.x - dx, y: r.y - dy };
+    let a = Coord {
+        x: r.x - dx,
+        y: r.y - dy,
+    };
+    let b = Coord {
+        x: r.x + dx,
+        y: r.y - dy,
+    };
 
-    let b = Coord { x: r.x + dx, y: r.y - dy };
-    v.push( Line {a: r.clone(), b: a});
-    v.push( Line {a: r, b});
+    v.push(Line { a: r.clone(), b: a });
+    v.push(Line { a: r, b });
 
-    tree_go(n - 1, v, theta - dtheta, branch_length, a);
-    tree_go(n - 1, v, theta - dtheta, branch_length, b);
+    for x in [a, b] {
+        tree_go(n - 1, v, theta - dtheta, branch_length, x)
+    }
 }
