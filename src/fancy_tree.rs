3e31ba3a9e1f95ed2ea5604bb2480238b5cd8360
amatgil <amatgilvinyes@gmail.com>
2024-04-12 18:38:18 +0200

    Finished the pyth tree

--- src/fancy_tree.rs
+++ src/fancy_tree.rs
@@ -4,15 +4,38 @@ use crate::*;
 const WIDTH: usize = 1500;
 const HEIGHT: usize = WIDTH;
 
+
+
+// Places in bounds
+fn normalize(c: Coord) -> Coord {
+    fn n(v: usize, w: usize) -> usize {
+        if v > 100000 { return 0; }
+        if v > w { return w - 1; }
+        return v;
+    }
+
+    Coord {
+        x: n(c.x, WIDTH),
+        y: n(c.y, HEIGHT),
+    }
+}
+
+fn mirror_y_properly(y: usize) -> usize {
+    let maybe = HEIGHT - y - 1;
+    if maybe > HEIGHT { 0 }
+    else { maybe } 
+}
+
 #[wasm_bindgen]
 pub fn pythagorean_tree(n: i32, theta: f64, side_length: usize) -> String {
     let mut shapes: Vec<Box<dyn Shape>> = Vec::new();
     let base_line = {
-        let base_line_h = HEIGHT * 30 / 100;
+        let base_line_h = HEIGHT * 15 / 100;
         let a = Coord { x: WIDTH / 2 - side_length / 2, y: base_line_h };
         let b = Coord { x: WIDTH / 2 + side_length / 2, y: base_line_h };
         Line { a, b }
     };
+    
 
     if n >= 0 {
         pythagorean_tree_go(n as usize, &mut shapes, theta, base_line);
@@ -21,14 +44,18 @@ pub fn pythagorean_tree(n: i32, theta: f64, side_length: usize) -> String {
     let mut buffer = String::new();
 
     buffer.push_str(&format!("<svg viewBox=\"0 0 {WIDTH} {HEIGHT} \" xmlns=\"http://www.w3.org/2000/svg\" id=\"pyth-tree-holder\">\n"));
+
     for shape in shapes {
         buffer.push_str(&format!("<polygon points=\""));
-        for Coord { x, y } in shape.points() {
-            let y = HEIGHT - y - 1;
-            buffer.push_str(&format!("{x},{y} "));
-        }
-        buffer.push_str(&format!("\" fill=\"none\" stroke=\"{COLOR}\"/>\n"));
+        for Coord { x, y } in shape.points().into_iter().map(normalize) { buffer.push_str(&format!("{x},{} ", mirror_y_properly(y))); }
+        buffer.push_str(&format!("\" fill=\"none\" stroke-width=\"5px\" stroke=\"{COLOR}\"/>\n"));
     }
+
+    buffer.push_str(&format!("<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke-width=\"10px\" stroke=\"#000000\" />",
+     base_line.a.x,
+     mirror_y_properly(base_line.a.y),
+     base_line.b.x,
+     mirror_y_properly(base_line.b.y)));
     buffer.push_str(&format!("</svg>\n"));
 
     return buffer;
@@ -59,14 +86,8 @@ fn pythagorean_tree_go(n: usize, shapes: &mut Shapes, theta: f64, base_line: Lin
         let a = q.d;
         let b = q.c;
         let c = {
-            //let dx = (l as f64 * theta.cos().powi(2)).round() as usize;
-            //let dy = (l as f64 * theta.sin().powi(2)).round() as usize;
-            //Coord {
-            //    x: base_line.a.x + dx,
-            //    y: base_line.a.y - dy,
-            //}
-            rotate_point_around_pivot(a, b, -theta)
-            //rotate_point_around_pivot(b, a, theta)
+            let extended = lerp_c(a, b, theta.cos());
+            rotate_point_around_pivot(extended, a, -theta)
         };
 
         Triangle { a, b, c }
---
+++ tests/sierp.rs
@@ -0,0 +1,5 @@
+#[test]
+fn basic() {
+    // This used a dbg and todo!() in the function itself. Useless, now
+    fractals::sierp(1);
+}
